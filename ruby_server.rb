91443154e94974648d312d838a09f5dd7f31da8e
Chris Jeon <chris0374@gmail.com>
2013-08-17 14:03:16 -0700

    Add ruby_server and ruby_server js

---
+++ ruby_server.rb
@@ -0,0 +1,20 @@
+require 'em-websocket'
+
+js_function = "newDevice({hi:1});"
+message = {stuff: js_function}
+EM.run {
+  EM::WebSocket.run(:host => "0.0.0.0", :port => 8080) do |ws|
+    ws.onopen { |handshake|
+      puts "WebSocket connection open"
+      puts handshake
+    }
+
+    ws.onclose { puts "Connection closed" }
+
+    ws.onmessage { |msg|
+      puts "Recieved message: #{msg}"
+      # ws.send "Pong: #{msg}"
+      ws.send ({method: "newDevice", args: 1}.to_json)
+    }
+  end
+}
